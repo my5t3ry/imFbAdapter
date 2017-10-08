0ff71154d8af0db0640d337ec88c254d6c57d807
my5t3ry <sash@mischkonsum.org>
2017-10-08 13:12:36 +0200

    refactoring

--- imgur/imgurScraper.py
+++ imgur/imgurScraper.py
@@ -26,26 +26,17 @@ class ImgurScraper(object):
 
         scheme, domain, path, param, query, fragment = urlparse(url)
         if domain == "i.imgur.com":
-            # Looks like a direct image already.
             return [url]
         paths = path[1:].split("/")
         if len(paths) == 1:
-            # No /a/, /gallery/ but could be imgur.com/qVhCPQB,5fLQ9KT
             ids = paths[0].split(",")
             img_urls = []
             for id_ in ids:
-                # imgur doesn't care if your url file ext is accurate.
                 path = id_ + ".jpg"
                 domain = "i.imgur.com"
                 img_urls.append(urlunparse((scheme, domain, path, None, None, None)))
         else:
-            # Scrape album, gallery pages.
             dom = self.lxmldom(url)
-            # Galleries don't always show an entire album, but surely you want the
-            #  entire album...
-            # full_album_url = dom.xpath(imgur_img_xpath)
-            # if full_album_url:
-            #     dom = url.lxmldom(full_album_url[0])
             img_urls = dom.xpath(imgur_img_xpath)
         return img_urls
 
